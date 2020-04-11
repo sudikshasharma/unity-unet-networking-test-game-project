8afbfde3f3c8fc84d7101f73ed92cc92090c046c
Sudiksha <sudiksha.sd1@gmail.com>
2020-04-11 06:07:32 +0530

    Implemented client-server based system

---
+++ Assets/Scripts/PlayerControlEnable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerControlEnable : NetworkBehaviour
+{
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            GetComponent<PlayerController>().enabled = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿//adapted from example script available at
+//https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        Quaternion turn = Quaternion.Euler(0, rotation, 0);
+        rb.MovePosition(rb.position + translation * this.transform.forward);
+        rb.MoveRotation(rb.rotation * turn);
+    }
+}
